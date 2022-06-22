d177c18ebe5a4554a63ea436474e8cd62bc2ecc8
Kamil Bajorek <60983182+KamilBajorek@users.noreply.github.com>
2022-06-22 20:24:50 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,1 @@
 <?php
-
-const USERNAME = 'ccojbiwqopukzc';
-const PASSWORD = 'd8eea9e9fb7a00a1f82bb4cd94885efb6a35b62a41fc9ad5527f4fc812e4d3b7';
-const HOST = 'ec2-34-242-8-97.eu-west-1.compute.amazonaws.com';
-const DATABASE = 'devq5229vkk60o';
